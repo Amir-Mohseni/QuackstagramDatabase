a7fa400a799c2feae1ed1a528942afb6db3f6af8
KoungMyatHein-UM <144337229+KoungMyatHein-UM@users.noreply.github.com>
2024-05-12 17:24:38 +0200

    Move table_creation.sql to appropriate directory
    The table_creation.sql file has been moved from a resources directory to a more fitting location within the Java package structure. This better organizes the project and places the SQL file closer to where it's likely to be used.

--- src/main/java/org/dacs/quackstagramdatabase/QUackstagramDatabaseApplication.java
+++ src/main/java/org/dacs/quackstagramdatabase/QUackstagramDatabaseApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QUackstagramDatabaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(QUackstagramDatabaseApplication.class, args);
+        SpringApplication.run(Handler.class, args);
     }
 
 }
