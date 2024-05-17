7da4735d359d371a1479c8725425ee51bf755466
Amirmohseni87 <amirmohseni87@yahoo.com>
2024-05-17 14:15:23 +0200

    Added the new dependencies

--- src/main/java/org/dacs/quackstagramdatabase/QuackstagramDatabaseApplication.java
+++ src/main/java/org/dacs/quackstagramdatabase/QuackstagramDatabaseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QUackstagramDatabaseApplication {
+public class QuackstagramDatabaseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Handler.class, args);
