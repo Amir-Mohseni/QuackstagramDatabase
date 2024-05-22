2642804a99726247b8e179ddca44abe672598b26
Amirmohseni87 <amirmohseni87@yahoo.com>
2024-05-22 18:17:16 +0200

    Changed FollowsEntity

--- src/main/java/org/dacs/quackstagramdatabase/database/entities/FollowsEntity.java
+++ src/main/java/org/dacs/quackstagramdatabase/database/entities/FollowsEntity.java
@@ -1,9 +1,13 @@
 package org.dacs.quackstagramdatabase.database.entities;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.dacs.quackstagramdatabase.database.annotations.Column;
 import org.dacs.quackstagramdatabase.database.annotations.Entity;
 import org.dacs.quackstagramdatabase.database.annotations.Id;
 
+@Setter
+@Getter
 @Entity(tableName = "Follows")
 public class FollowsEntity {
     @Id
