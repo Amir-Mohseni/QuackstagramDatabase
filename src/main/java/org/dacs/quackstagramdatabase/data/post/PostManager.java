95e4e14ab13371c566f0dfad36cd49e41b8a9979
Amirmohseni87 <amirmohseni87@yahoo.com>
2024-05-23 18:26:35 +0200

    Added img folder

---
+++ src/main/java/org/dacs/quackstagramdatabase/data/post/PostManager.java
@@ -0,0 +1,69 @@
+package org.dacs.quackstagramdatabase.data.post;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.dacs.quackstagramdatabase.Handler;
+import org.dacs.quackstagramdatabase.data.user.User;
+import org.dacs.quackstagramdatabase.database.DatabaseConfig;
+import org.dacs.quackstagramdatabase.database.EntityManager;
+import org.dacs.quackstagramdatabase.database.entities.PostEntity;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+public class PostManager {
+    private HashMap<Integer, Post> posts;
+
+    public PostManager(){
+        this.posts = new HashMap<>();
+        load();
+    }
+
+    public void postPost(User user, Post post){
+        posts.put(post.getPostID(), post);
+        post.setPostedByUsername(user.getUsername());
+
+        try {
+            EntityManager em = new EntityManager(new DatabaseConfig());
+
+            em.persist(new PostEntity(user.getUsername(), post.getCaption(), post.getExtension()));
+
+        } catch (SQLException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Post getByPostID(Integer postID){
+        return posts.get(postID);
+    }
+
+    public List<Post> getAsList(){
+        return new ArrayList<>(posts.values());
+    }
+
+    private void load(){
+        try {
+            EntityManager em = new EntityManager(new DatabaseConfig());
+
+            List<PostEntity> postEntities = em.findAll(PostEntity.class);
+
+            for (PostEntity postEntity : postEntities) {
+                Post post = new Post(postEntity.getPostId(), postEntity.getUsername(), postEntity.getCaption(), postEntity.getMediaUrl());
+                posts.put(post.getPostID(), post);
+            }
+        } catch (SQLException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
