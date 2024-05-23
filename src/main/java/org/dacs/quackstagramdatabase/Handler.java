b1f78adeeb4a711f0595bb7c56cdf33f7ef43000
Amirmohseni87 <amirmohseni87@yahoo.com>
2024-05-23 23:46:24 +0200

    idk

--- src/main/java/org/dacs/quackstagramdatabase/Handler.java
+++ src/main/java/org/dacs/quackstagramdatabase/Handler.java
@@ -5,6 +5,7 @@ import lombok.extern.apachecommons.CommonsLog;
 import org.dacs.quackstagramdatabase.data.DataManager;
 import org.dacs.quackstagramdatabase.ui.UIManager;
 import org.dacs.quackstagramdatabase.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,6 +20,7 @@ public class Handler {
     @Getter
     private static Util util;
 
+    @Autowired
     public Handler(DataManager dataManager) {
         this.dataManager = dataManager;
     }
