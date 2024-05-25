6d2b758a827b9ee0fc6b232e1139b28d90d950e4
Amirmohseni87 <amirmohseni87@yahoo.com>
2024-05-25 19:00:49 +0200

    Added procedures to triggers

--- src/main/resources/sql/triggers.sql
+++ src/main/resources/sql/triggers.sql
@@ -1,75 +1,65 @@
 DELIMITER $$
-CREATE TRIGGER after_post_insert
-AFTER INSERT ON Posts
-FOR EACH ROW
+
+-- Create a procedure to update follower/following counts
+CREATE PROCEDURE UpdateFollowCounts(
+    IN followerUsername VARCHAR(255),
+    IN followedUsername VARCHAR(255),
+    IN operation VARCHAR(10) -- 'INCREMENT' or 'DECREMENT'
+)
 BEGIN
-    UPDATE Users
-    SET PostsCount = PostsCount + 1
-    WHERE Username = NEW.Username;
-END$$
-DELIMITER ;
+    IF operation = 'INCREMENT' THEN
+        -- Increment the number of followers for the followed user
+        UPDATE Users
+        SET NumFollowers = NumFollowers + 1
+        WHERE Username = followedUsername;
 
-DELIMITER $$
-CREATE TRIGGER after_comment_insert
-AFTER INSERT ON Comments
-FOR EACH ROW
+        -- Increment the number of following for the follower user
+        UPDATE Users
+        SET NumFollowing = NumFollowing + 1
+        WHERE Username = followerUsername;
+    ELSEIF operation = 'DECREMENT' THEN
+        -- Decrement the number of followers for the followed user
+        UPDATE Users
+        SET NumFollowers = NumFollowers - 1
+        WHERE Username = followedUsername;
+
+        -- Decrement the number of following for the follower user
+        UPDATE Users
+        SET NumFollowing = NumFollowing - 1
+        WHERE Username = followerUsername;
+    END IF;
+END$$
+
+-- Create a function to get the number of posts a user has liked
+CREATE FUNCTION GetNumLikedPosts(userUsername VARCHAR(255))
+    RETURNS INT
 BEGIN
-    UPDATE Posts
-    SET NumComments = NumComments + 1
-    WHERE PostID = NEW.PostID;
+    DECLARE numLikedPosts INT DEFAULT 0;
+
+    SELECT COUNT(*)
+    INTO numLikedPosts
+    FROM Likes
+    WHERE Username = userUsername;
+
+    RETURN numLikedPosts;
 END$$
-DELIMITER ;
 
-DELIMITER $$
-CREATE TRIGGER after_like_insert
-AFTER INSERT ON Likes
-FOR EACH ROW
-BEGIN
-    UPDATE Posts
-    SET NumLikes = NumLikes + 1
-    WHERE PostID = NEW.PostID;
-END$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE TRIGGER before_follow_insert
-BEFORE INSERT ON Follows
-FOR EACH ROW
-BEGIN
-    SET NEW.FollowDate = CURRENT_TIMESTAMP;
-END$$
-DELIMITER ;
-
-DELIMITER $$
-
+-- Trigger after inserting a follow to update follow counts and call the procedure
 CREATE TRIGGER after_follow_insert
-AFTER INSERT ON Follows
-FOR EACH ROW
+    AFTER INSERT ON Follows
+    FOR EACH ROW
 BEGIN
-    -- Update the number of followers for the followed user
-    UPDATE Users
-    SET NumFollowers = NumFollowers + 1
-    WHERE Username = NEW.FollowedUsername;
-
-    -- Update the number of following for the follower user
-    UPDATE Users
-    SET NumFollowing = NumFollowing + 1
-    WHERE Username = NEW.FollowerUsername;
+    -- Call the procedure to update follow counts
+    CALL UpdateFollowCounts(NEW.FollowerUsername, NEW.FollowedUsername, 'INCREMENT');
 END$$
 
+-- Trigger after deleting a follow to update follow counts and call the procedure
 CREATE TRIGGER after_follow_delete
-AFTER DELETE ON Follows
-FOR EACH ROW
+    AFTER DELETE ON Follows
+    FOR EACH ROW
 BEGIN
-    -- Update the number of followers for the unfollowed user
-    UPDATE Users
-    SET NumFollowers = NumFollowers - 1
-    WHERE Username = OLD.FollowedUsername;
-
-    -- Update the number of following for the unfollower user
-    UPDATE Users
-    SET NumFollowing = NumFollowing - 1
-    WHERE Username = OLD.FollowerUsername;
+    -- Call the procedure to update follow counts
+    CALL UpdateFollowCounts(OLD.FollowerUsername, OLD.FollowedUsername, 'DECREMENT');
 END$$
 
 DELIMITER ;
